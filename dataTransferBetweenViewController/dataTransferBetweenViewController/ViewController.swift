7d6e431b47628ee5acaab00032abfbf43bff4e0a
kidawa <thefijimonster@gmail.com>
2018-07-22 14:26:03 +0100

    Sunday 22 July 14:26

--- dataTransferBetweenViewController/dataTransferBetweenViewController/ViewController.swift
+++ dataTransferBetweenViewController/dataTransferBetweenViewController/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var termField: UITextField!
+    
+    var segueText = ""
+    var segueTermText = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func done(_ sender: Any)
+    {
+        self.segueText = textField.text! 
+        self.segueTermText = termField.text!
+        performSegue(withIdentifier: "segue", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let vc = segue.destination as! secondViewController
+        vc.finalSegue = self.segueText
+        vc.finalfSegue = self.segueTermText
+    }
+    
 }
 
