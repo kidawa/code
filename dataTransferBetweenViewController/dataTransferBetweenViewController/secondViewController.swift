f0672148408a9e2b960c39b62f090be7918253fa
kidawa <thefijimonster@gmail.com>
2018-07-22 14:24:41 +0100

    Sunday 22 July 14:25:00

--- dataTransferBetweenViewController/dataTransferBetweenViewController/secondViewController.swift
+++ dataTransferBetweenViewController/dataTransferBetweenViewController/secondViewController.swift
@@ -9,27 +9,32 @@
 import UIKit
 
 class secondViewController: UIViewController {
-
+    
+    var finalSegue = ""
+    var finalfSegue = ""
+    
+    @IBOutlet weak var button: UIButton!
+    var isOpen = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonFlip(_ sender: Any)
+    {
+        if isOpen {
+            isOpen = false
+            let frontSide = finalSegue
+            button.setTitle(frontSide, for: .normal)
+            UIView.transition(with: button, duration: 0.3, options: UIViewAnimationOptions.transitionFlipFromLeft, animations: nil, completion: nil)
+        }
+        else {
+            isOpen = true
+            let frontSide = finalfSegue
+            button.setTitle(frontSide, for: .normal)
+            UIView.transition(with: button, duration: 0.3, options: UIViewAnimationOptions.transitionFlipFromRight, animations: nil, completion: nil)
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
